e956fb7a12d8dda855326c54d8e92cc33e8a2f00
crazyStrongboy <13237124836@163.com>
2019-01-14 16:37:12 +0800

    添加注释

--- echo/src/main/java/github/com/crazyStrongboy/netty/server/NettyServer.java
+++ echo/src/main/java/github/com/crazyStrongboy/netty/server/NettyServer.java
@@ -17,15 +17,19 @@ public class NettyServer {
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(bossGroup, workerGroup)
+                    //设置创建channel的工厂，后面会调用NioServerSocketChannel的无参构造进行初始化，此处不初始化NioServerSocketChannel
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<Channel>() {
                         @Override
                         public void initChannel(final Channel ch) throws Exception {
                             final ChannelPipeline pipeline = ch.pipeline();
+                            //这里可以追加很多自定义handler
                             pipeline.addLast(new ServerHandler());
                         }
                     })
+                    //服务端将不能处理的客户端连接请求放在队列中等待处理，backlog参数指定了队列的大小
                     .option(ChannelOption.SO_BACKLOG, 128)
+                    //当设置该选项以后，如果在两小时内没有数据的通信时，TCP会自动发送一个活动探测数据报文。
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
             ChannelFuture future = bootstrap.bind(Global.PORT).sync();
             System.err.println("服务端开始监听。。。。" + Global.PORT);
