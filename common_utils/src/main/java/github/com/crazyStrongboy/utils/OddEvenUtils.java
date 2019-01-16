487d07a72df37c0062140d2925da4e93fb31a0ce
crazyStrongboy <13237124836@163.com>
2019-01-16 19:32:07 +0800

    test

---
+++ common_utils/src/main/java/github/com/crazyStrongboy/utils/OddEvenUtils.java
@@ -0,0 +1,21 @@
+package github.com.crazyStrongboy.utils;
+
+public class OddEvenUtils {
+    /**
+     * 判断是否是偶数
+     * @param num
+     * @return
+     */
+    public static boolean isOdd(Integer num) {
+        return (num & 1) == 0;
+    }
+
+    /**
+     * 判断是否是奇数
+     * @param num
+     * @return
+     */
+    public static boolean isEven(Integer num) {
+        return (num & 1) == 1;
+    }
+}
---
+++ common_utils/src/test/java/Test.java
@@ -0,0 +1,8 @@
+import github.com.crazyStrongboy.utils.OddEvenUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean b = OddEvenUtils.isEven(5);
+        System.out.println(b);
+    }
+}
